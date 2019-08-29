a6978d84dcb7981c930694fd6d49194aa88a4a23
Orlando Carnate <orlando.carnate@gmail.com>
2019-08-29 01:45:33 -0500

    more renderings

--- src/renderings.js
+++ src/renderings.js
@@ -1,5 +1,50 @@
 export const photos = [
     {
+      src: "./images/architectural/Comfort-Inn-Exterior.jpg",
+      width: 16,
+      height: 9
+    },
+    {
+      src: "./images/architectural/Comfort-Suites-Exterior.jpg",
+      width: 16,
+      height: 9
+    },
+    {
+      src: "./images/architectural/Condo-Night.jpg",
+      width: 16,
+      height: 9
+    },
+    {
+      src: "./images/architectural/Condo-View-2-Daylight.jpg",
+      width: 16,
+      height: 9
+    },
+    {
+      src: "./images/architectural/Condo-Wire.jpg",
+      width: 16,
+      height: 9
+    },
+    {
+      src: "./images/architectural/Hotel-Lobby-Interior.jpg",
+      width: 16,
+      height: 9
+    },
+    {
+      src: "./images/architectural/Residential-Interior.jpg",
+      width: 16,
+      height: 9
+    },
+    {
+      src: "./images/architectural/Office-Lobby-Interior.jpg",
+      width: 16,
+      height: 9
+    },
+    {
+      src: "./images/architectural/parkhomeskitchen.jpg",
+      width: 16,
+      height: 9
+    },
+    {
       src: "./images/interior/Autonomy-interior-Option-2-birdseye.jpg",
       width: 16,
       height: 9

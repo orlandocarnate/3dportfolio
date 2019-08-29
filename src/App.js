bb8546580fc06f2e1399b09edbaf3172c6e5a6cb
Orlando Carnate <orlando.carnate@gmail.com>
2019-08-28 19:22:19 -0500

    added renderings, bootstrap, navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback } from "react";
+// import { render } from "react-dom";
+import Gallery from "react-photo-gallery";
+import Carousel, { Modal, ModalGateway } from "react-images";
+import { photos } from "./renderings";
 
 function App() {
+  const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a className="navbar-brand" href="/">Carnate3D Renderings</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarColor01">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="https://orlandocarnate.com">Developer Page<span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="https://carnate3d.wordpress.com">Blog</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="https://www.linkedin.com/in/orlando-carnate/">LinkedIn</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="https://github.com/orlandocarnate">GitHub</a>
+            </li>
+          </ul>
+          <form className="form-inline my-2 my-lg-0">
+            <input className="form-control mr-sm-2" type="text" placeholder="Search" />
+            <button className="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
+
+      <section>
+        <Gallery photos={photos} onClick={openLightbox} />
+        <ModalGateway>
+          {viewerIsOpen ? (
+            <Modal onClose={closeLightbox}>
+              <Carousel
+                currentIndex={currentImage}
+                views={photos.map(x => ({
+                  ...x,
+                  srcset: x.srcSet,
+                  caption: x.title
+                }))}
+              />
+            </Modal>
+          ) : null}
+        </ModalGateway>
+
+      </section>
     </div>
   );
 }
---
+++ src/renderings.js
@@ -0,0 +1,83 @@
+export const photos = [
+    {
+      src: "./images/AAPM-20x40-birdseye.jpg",
+      width: 16,
+      height: 9
+    },
+    {
+      src: "./images/Broadwind-20x20-B-view2.jpg",
+      width: 16,
+      height: 9
+    },
+    {
+      src: "./images/Dealer-E-20x20-A1-view-2.jpg",
+      width: 16,
+      height: 9
+    },
+    {
+      src: "./images/HP-Idol-On-Demand-20x20-A-view1.jpg",
+      width: 16,
+      height: 9
+    },
+    {
+      src: "./images/Nuix-Blackhat-20x30-birdseye.jpg",
+      width: 16,
+      height: 9
+    },
+    {
+      src: "./images/OpenText-HIMSS-20x20-B-right-corner-view.jpg",
+      width: 16,
+      height: 9
+    },
+    {
+      src: "./images/Plum-30x30-view2.jpg",
+      width: 16,
+      height: 9
+    },
+    {
+      src: "./images/Plum-Organics-30x30-D-corner.jpg",
+      width: 16,
+      height: 9
+    },
+    {
+      src: "./images/QUT-20x20-B-birdseye.jpg",
+      width: 16,
+      height: 9
+    },
+    {
+      src: "./images/Schumacher-20x40-view2.jpg",
+      width: 16,
+      height: 9
+    },
+    {
+      src: "./images/Shick-20x30-C1-birdseye.jpg",
+      width: 16,
+      height: 9
+    },
+    {
+      src: "./images/Smartbrew-20x20-B-front.jpg",
+      width: 16,
+      height: 9
+    },
+    {
+      src: "./images/Smartbrew-20x20-front.jpg",
+      width: 16,
+      height: 9
+    },
+    {
+      src: "./images/Univar-20x20-B-view-1.jpg",
+      width: 16,
+      height: 9
+    },
+    {
+      src: "./images/Valley-20x20-birdseye.jpg",
+      width: 16,
+      height: 9
+    },
+    {
+      src: "./images/Varian-20x20-A-side-view.jpg",
+      width: 16,
+      height: 9
+    }
+  ];
+  

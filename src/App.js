3aa1358bab84ef09e7ddf984162c300d1a0fcfda
Orlando Carnate <orlando.carnate@gmail.com>
2019-08-31 03:19:15 -0500

    incorporated react-router, add dropdown to navbar

--- src/App.js
+++ src/App.js
@@ -1,72 +1,25 @@
 import React, { useState, useCallback } from "react";
 // import { render } from "react-dom";
-import Gallery from "react-photo-gallery";
-import Carousel, { Modal, ModalGateway } from "react-images";
-import { photos } from "./renderings";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import NavBar from "./components/navbar";
+import Home from "./pages/Home";
+import Architecture from "./pages/Architecture";
+import Tradeshows from "./pages/Tradeshows";
 
 function App() {
-  const [currentImage, setCurrentImage] = useState(0);
-  const [viewerIsOpen, setViewerIsOpen] = useState(false);
-
-  const openLightbox = useCallback((event, { photo, index }) => {
-    setCurrentImage(index);
-    setViewerIsOpen(true);
-  }, []);
-
-  const closeLightbox = () => {
-    setCurrentImage(0);
-    setViewerIsOpen(false);
-  };
-
   return (
-    <div className="container">
-      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-        <a className="navbar-brand" href="/">Carnate3D Renderings</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
+    <Router>
+      <div className="container">
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/Architecture" component={Architecture} />
+          <Route exact path="/Tradeshows" component={Tradeshows} />
+        </Switch>
 
-        <div className="collapse navbar-collapse" id="navbarColor01">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <a className="nav-link" href="https://orlandocarnate.com">Developer Page<span className="sr-only">(current)</span></a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="https://carnate3d.wordpress.com">Blog</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="https://www.linkedin.com/in/orlando-carnate/">LinkedIn</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="https://github.com/orlandocarnate">GitHub</a>
-            </li>
-          </ul>
-          {/* <form className="form-inline my-2 my-lg-0">
-            <input className="form-control mr-sm-2" type="text" placeholder="Search" />
-            <button className="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-          </form> */}
-        </div>
-      </nav>
-
-      <section>
-        <Gallery photos={photos} onClick={openLightbox} />
-        <ModalGateway>
-          {viewerIsOpen ? (
-            <Modal onClose={closeLightbox}>
-              <Carousel
-                currentIndex={currentImage}
-                views={photos.map(x => ({
-                  ...x,
-                  srcset: x.srcSet,
-                  caption: x.title
-                }))}
-              />
-            </Modal>
-          ) : null}
-        </ModalGateway>
-
-      </section>
-    </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/navbar.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+function NavBar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+            <a className="navbar-brand" href="/">Orlando Carnate's 3D Portfolio</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarColor01">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item dropdown">
+                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Renderings</a>
+                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a className="dropdown-item" href="/Tradeshows">Tradeshows & Exhibits</a>
+                            <a className="dropdown-item" href="/Architecture">Architecture</a>
+                            <div className="dropdown-divider"></div>
+                            <a className="dropdown-item disabled" href="#">Animations</a>
+                        </div>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="https://orlandocarnate.com">Developer Page<span className="sr-only"></span></a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="https://carnate3d.wordpress.com">Blog</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="https://www.linkedin.com/in/orlando-carnate/">LinkedIn</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="https://github.com/orlandocarnate">GitHub</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar;

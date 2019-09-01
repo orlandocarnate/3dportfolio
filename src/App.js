7c2b4719931a54f72b277b308933193684883617
Orlando Carnate <orlando.carnate@gmail.com>
2019-09-01 02:41:00 -0500

    point to Carnate3d.com

--- src/App.js
+++ src/App.js
@@ -1,61 +1,45 @@
-import React, { useState, useCallback } from "react";
-import Gallery from "react-photo-gallery";
-import Carousel, { Modal, ModalGateway } from "react-images";
-import photos from "./components/renderings";
-// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import NavBar from "./components/navbar";
-// import Home from "./pages/Home";
-// import Architecture from "./pages/Architecture";
-// import Tradeshows from "./pages/Tradeshows";
+import Home from "./pages/Home";
+import Architecture from "./pages/Architecture";
+import Tradeshows from "./pages/Tradeshows";
 
 function App() {
-  const [currentImage, setCurrentImage] = useState(0);
-  const [viewerIsOpen, setViewerIsOpen] = useState(false);
-
-  const openLightbox = useCallback((event, { photo, index }) => {
-    setCurrentImage(index);
-    setViewerIsOpen(true);
-  }, []);
-
-  const closeLightbox = () => {
-    setCurrentImage(0);
-    setViewerIsOpen(false);
-  };
-
   return (
-    // <Router>
-    //   <div className="container">
-    //     <Switch>
-    //       <Route exact path="/" component={Home} />
-    //       <Route path="/Architecture" component={Architecture} />
-    //       <Route path="/Tradeshows" component={Tradeshows} />
-    //     </Switch>
+    <Router>
+      <div className="container">
+      <NavBar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/Architecture" component={Architecture} />
+          <Route path="/Tradeshows" component={Tradeshows} />
+        </Switch>
 
-    //   </div>
-    // </Router>
-    <div className="container">
-      <section>
-        <NavBar />
-        <Gallery photos={photos} onClick={openLightbox} />
+      </div>
+    </Router>
+    // <div className="container">
+    //   <section>
+    //     <Gallery photos={photos} onClick={openLightbox} />
 
-        <ModalGateway>
-          {viewerIsOpen ? (
-            <Modal onClose={closeLightbox}>
-              <Carousel
-                currentIndex={currentImage}
-                views={photos.map(x => ({
-                  ...x,
-                  srcset: x.srcSet,
-                  caption: x.title
-                }))}
-              />
-            </Modal>
-          ) : null}
-        </ModalGateway>
+    //     <ModalGateway>
+    //       {viewerIsOpen ? (
+    //         <Modal onClose={closeLightbox}>
+    //           <Carousel
+    //             currentIndex={currentImage}
+    //             views={photos.map(x => ({
+    //               ...x,
+    //               srcset: x.srcSet,
+    //               caption: x.title
+    //             }))}
+    //           />
+    //         </Modal>
+    //       ) : null}
+    //     </ModalGateway>
 
-      </section>
-    </div>
+    //   </section>
+    // </div>
 
   );
 }

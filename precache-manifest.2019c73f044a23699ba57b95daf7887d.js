1f99f1725af08761dbbb38e4e2c6db3464909fac
Orlando Carnate <orlando.carnate@gmail.com>
2019-09-01 16:19:50 -0500

    Updates

---
+++ precache-manifest.2019c73f044a23699ba57b95daf7887d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a29c5f33c0c9b4c01ac3753bf85cb9de",
+    "url": "/3dportfolio/index.html"
+  },
+  {
+    "revision": "f2b08a60e94bac8b4ce9",
+    "url": "/3dportfolio/static/js/2.f65d034c.chunk.js"
+  },
+  {
+    "revision": "01afb4c1dd87bdc5bf33",
+    "url": "/3dportfolio/static/js/main.5f0d55c7.chunk.js"
+  },
+  {
+    "revision": "b591d6a100d5add355bb",
+    "url": "/3dportfolio/static/js/runtime~main.c88d3c51.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1d02b52888559a9c689b6fe5e7996d15.js"
+  "/3dportfolio/precache-manifest.2019c73f044a23699ba57b95daf7887d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/3dportfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

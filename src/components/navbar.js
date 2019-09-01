78388d5e7fc1034004d02297fd7f988964a00ee5
Orlando Carnate <orlando.carnate@gmail.com>
2019-09-01 12:40:25 -0500

    fixed collapsible navbar menu

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,7 +4,7 @@ function NavBar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
             <a className="navbar-brand" href="/">Orlando Carnate's 3D Portfolio</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
 

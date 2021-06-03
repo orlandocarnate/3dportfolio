94a3a8fb09982e06a23e9fa1ec4055c290887d6c
Orlando Carnate <orlando.carnate@gmail.com>
2021-06-02 22:09:40 -0500

    fix navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,24 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Nav, Navbar, Container } from 'react-bootstrap'
+import { Nav, Navbar, Container } from "react-bootstrap";
 
 function NavBar() {
   return (
-    <Navbar className="navbar-expand-lg navbar-dark bg-primary">
-      <Navbar.Brand href="./">
-        Orlando Carnate
-      </Navbar.Brand>
-      <Nav className="me-auto">
+    <Navbar collapseOnSelect expand="lg" className="navbar-expand-lg navbar-dark bg-primary">
+      <Navbar.Brand href="./">Orlando Carnate</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="me-auto">
         <Nav.Link href="./#/">Home</Nav.Link>
-        <Nav.Link href="./#/Threejs">Three.js Projects</Nav.Link>
-        <Nav.Link href="./#/VRTours">VR Tours</Nav.Link>
-        <Nav.Link href="./#/Architecture">Architecture</Nav.Link>
-        <Nav.Link href="./#/Tradeshows">Tradeshows & Exhibits</Nav.Link>
-        <Nav.Link href="https://www.youtube.com/playlist?list=PLv0bey_3GrtIx5w71fSIR25fJ38iyN_11"><i className="fab fa-youtube"></i> Animations</Nav.Link>
-        <Nav.Link href="https://www.linkedin.com/in/orlando-carnate/"><i className="fab fa-linkedin"></i> LinkedIn</Nav.Link>
-        <Nav.Link href="https://github.com/orlandocarnate"><i className="fab fa-github"></i>GitHub</Nav.Link>
-      </Nav>
-
+          <Nav.Link href="./#/Threejs">Three.js Projects</Nav.Link>
+          <Nav.Link href="./#/VRTours">VR Tours</Nav.Link>
+          <Nav.Link href="./#/Architecture">Architecture</Nav.Link>
+          <Nav.Link href="./#/Tradeshows">Tradeshows & Exhibits</Nav.Link>
+          <Nav.Link href="https://www.youtube.com/playlist?list=PLv0bey_3GrtIx5w71fSIR25fJ38iyN_11">
+            <i className="fab fa-youtube"></i> Animations
+          </Nav.Link>
+          <Nav.Link href="https://www.linkedin.com/in/orlando-carnate/">
+            <i className="fab fa-linkedin"></i> LinkedIn
+          </Nav.Link>
+          <Nav.Link href="https://github.com/orlandocarnate">
+            <i className="fab fa-github"></i>GitHub
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 }

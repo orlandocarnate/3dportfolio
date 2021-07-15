4d2f3dc8e6c278aa9873c7b9d6d1c35cff08a247
Orlando Carnate <orlando.carnate@gmail.com>
2021-07-15 00:36:50 -0500

    update layout, added ar-vr section

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -8,20 +8,25 @@ function NavBar() {
       <Navbar.Brand href="./">Orlando Carnate</Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="me-auto">
-        <Nav.Link href="./#/">Home</Nav.Link>
-          <Nav.Link href="./#/Threejs">Three.js Projects</Nav.Link>
-          <Nav.Link href="./#/VRTours">VR Tours</Nav.Link>
-          <Nav.Link href="./#/Architecture">Architecture</Nav.Link>
-          <Nav.Link href="./#/Tradeshows">Tradeshows & Exhibits</Nav.Link>
-          <Nav.Link href="https://www.youtube.com/playlist?list=PLv0bey_3GrtIx5w71fSIR25fJ38iyN_11">
-            <i className="fab fa-youtube"></i> Animations
+        <Nav className="container-fluid">
+          <Nav.Link href="./#/">Home</Nav.Link>
+            <Nav.Link href="./#/Threejs">Three.js Projects</Nav.Link>
+            <Nav.Link href="https://youtube.com/playlist?list=PLv0bey_3GrtI6Dcr5oUqkjlnxiiRPWL6T">
+              <i className="fab fa-youtube"></i> VR/AR Demos
+            </Nav.Link>
+            <Nav.Link href="https://www.youtube.com/playlist?list=PLv0bey_3GrtIx5w71fSIR25fJ38iyN_11">
+              <i className="fab fa-youtube"></i> Animations
+            </Nav.Link>
+            <Nav.Link href="./#/VRTours">VR Tours</Nav.Link>
+            <Nav.Link href="./#/Architecture">Architecture</Nav.Link>
+            <Nav.Link href="./#/Tradeshows">Tradeshows & Exhibits</Nav.Link>
+        </Nav>
+        <Nav>
+          <Nav.Link href="https://www.linkedin.com/in/orlando-carnate/" className="d-flex align-items-center">
+            <i className="fab fa-linkedin"></i>&nbsp;LinkedIn
           </Nav.Link>
-          <Nav.Link href="https://www.linkedin.com/in/orlando-carnate/">
-            <i className="fab fa-linkedin"></i> LinkedIn
-          </Nav.Link>
-          <Nav.Link href="https://github.com/orlandocarnate">
-            <i className="fab fa-github"></i>GitHub
+          <Nav.Link href="https://github.com/orlandocarnate" className="d-flex align-items-center">
+            <i className="fab fa-github"></i>&nbsp;GitHub
           </Nav.Link>
         </Nav>
       </Navbar.Collapse>
